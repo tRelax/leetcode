be146bb2babe8abfaeb454d6588bd1d4155d2df1
tRelax <tinmil7@gmail.com>
2024-12-08 23:45:37 +0100

    [Easy] Completed "Same Binary Tree"

---
+++ Trees/is_same_tree.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from trees_helpers import TreeNode
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
