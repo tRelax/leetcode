0a5d5e8c92d930f2c0946b60d9bdf5a0947caa66
tRelax <tinmil7@gmail.com>
2024-12-08 23:18:01 +0100

    [Easy] Completed "Maximum Depth of Binary Tree"

--- Trees/max_depth.py
+++ Trees/max_depth.py
@@ -1,14 +1,45 @@
 from typing import Optional
 from trees_helpers import *
 
-# DFS recursive
-
 
 class Solution:
+    # DFS recursive
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+    # BFS
+    def maxDepthBFS(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+
+        return level
+
+    # Iterative DFS
+    def maxDepthIterativeDFS(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
 
 
 root = create_tree_from_list([1, 2, 3, None, None, 4])
