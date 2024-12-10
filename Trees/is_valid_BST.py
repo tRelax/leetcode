deb67372e44185fdb5d98a3ab1cd8d5f1a0cd30f
tRelax <tinmil7@gmail.com>
2024-12-10 22:58:29 +0100

    [Medium] Completed "Valid Binary Search Tree"

---
+++ Trees/is_valid_BST.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from trees_helpers import *
+
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, left, right):
+            if not node:
+                return True
+
+            if not (left < node.val < right):
+                return False
+
+            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+
+        return valid(root, float("-inf"), float("inf"))
