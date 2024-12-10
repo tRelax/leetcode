f2e9240ee5de95dc5b1188d7e2bc4535b66445ba
tRelax <tinmil7@gmail.com>
2024-12-10 22:35:03 +0100

    [Medium] Completed "Count Good Nodes in Binary Tree"

---
+++ Trees/good_nodes.py
@@ -0,0 +1,17 @@
+from trees_helpers import *
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
