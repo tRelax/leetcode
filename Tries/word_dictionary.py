0ae500087f0ae67b2cbe602b18023b1b9c4b0792
tRelax <tinmil7@gmail.com>
2024-12-11 21:51:13 +0100

    [Medium] Completed "Design Add and Search Word Data Structure"

---
+++ Tries/word_dictionary.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root: TrieNode):
+            cur = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.endOfWord
+
+        return dfs(0, self.root)
+
+
+wordDictionary = WordDictionary()
+wordDictionary.addWord("day")
+wordDictionary.addWord("bay")
+wordDictionary.addWord("may")
+print(wordDictionary.search("say"))
+print(wordDictionary.search("day"))
+print(wordDictionary.search(".ay"))
+print(wordDictionary.search("b.."))
