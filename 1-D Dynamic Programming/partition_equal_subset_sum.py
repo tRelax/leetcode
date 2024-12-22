09b71f66593c4cfb6f5c0e17743a22383c872a06
tRelax <tinmil7@gmail.com>
2024-12-22 14:02:42 +0100

    [Medium] Completed "Partition Equal Subset Sum"

---
+++ 1-D Dynamic Programming/partition_equal_subset_sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if sum(nums) % 2:
+            return False
+
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
+
+        for i in range(len(nums) - 1, -1, -1):
+            nextDp = set()
+            for t in dp:
+                if t + nums[i] == target:
+                    return True
+                nextDp.add(t+nums[i])
+                nextDp.add(t)
+            dp = nextDp
+        return False
+
+
+nums = [1, 2, 3, 4]
+print(Solution().canPartition(nums))
+nums = [1, 2, 3, 4, 5]
+print(Solution().canPartition(nums))
