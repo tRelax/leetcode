c02466acfab5d4fc641bd3a9b708c99f5ac1cbbf
tRelax <tinmil7@gmail.com>
2025-02-05 22:00:50 +0100

    [Medium] Completed "Best Time to Buy and Sell Stock with Cooldown"

---
+++ 2-D Dynamid Programming/best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0
+
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+
+            cooldown = dfs(i+1, buying)
+            if buying:
+                buy = dfs(i+1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i+2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
+            return dp[(i, buying)]
+
+        return dfs(0, True)
+
+
+prices = [1, 3, 4, 0, 4]
+print(Solution().maxProfit(prices))
